20690c0e01dba7735806ad066172e2efe4b85d34
waeliFatima <44834039+waeliFatima@users.noreply.github.com>
2018-11-10 00:36:51 +0700

    Add files via upload

---
+++ models/cart.js
@@ -0,0 +1,29 @@
+module.exports = function Cart(oldCart) {
+    this.items =oldCart.items || {} ;
+    this.totalQty = oldCart.totalQty || 0;
+    this.totalPrice = oldCart.totalPrice || 0;
+
+    this.add=function (item,id) {
+        var storedItem=this.items[id];
+        if(!storedItem){
+            storedItem = this.items[id]= {item :item , qty:0,price:0};
+        }
+        storedItem.qty++;
+        storedItem.price = storedItem.item.price * storedItem.qty;
+        this.totalQty++;
+        this.totalPrice += storedItem.item.price;
+    };
+    this.reduceByOne= function (id) {
+        this.items[id].qty--;
+        this.items[id].price -= this.items[id].items.price;
+        this.totalQty--;
+        this.totalPrice -= this.items[id].items.price;
+    };
+    this.generateArray=function () {
+        var arr =[];
+        for(var id in this.items){
+            arr.push(this.items[id]);
+        }
+        return arr;
+    };
+};
